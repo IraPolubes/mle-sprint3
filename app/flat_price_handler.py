2b8fb1bdafa1fe78e110f3326ec508ec53f38064
Irina Poloubessov <polubes@gmail.com>
2024-05-21 11:54:02 +0200

    started API

--- app/flat_price_handler.py
+++ app/flat_price_handler.py
@@ -1,5 +1,5 @@
 
-import joblib
+from joblib import load
 
 """
 pipeline = Pipeline([
@@ -17,11 +17,26 @@ prepared_features = pipeline.transform(required_data)
 
 """
 
+def remove_outliers(df, num_cols):  # для входящих тестовых данных
+    threshold = 1.5
+    for col in num_cols:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        margin = threshold * IQR
+        lower = Q1 - margin
+        upper = Q3 + margin
+        mask = df[col].between(lower, upper)
+        df = df[mask]
+
+
 class FlatPriceHandler:
     """Класс Handler, который обрабатывает запрос и возвращает предсказание."""
     def __init__(self):
-        self.model_path = ''
-        self.model = None # до тех пор пока не проинициализирован
+        self.model_path = 'models/my_model.joblib'
+        self.pipeline_path = 'models/my_pipeline.joblib'
+        self.model = None # Объявление полей класса централизовано и заранее в одном месте для ясности
+        self.pipeline = None
 
         # нам не нужен studio, is_apartment, rooms, living_area, оставляем только те что использованы в обучении
         self.required_model_params = [
@@ -29,12 +44,14 @@ class FlatPriceHandler:
             'building_type_int', 'latitude', 'longitude', 'ceiling_height', 'flats_count', 'floors_total',
             'has_elevator'
         ]
+        self.num_cols = list(set(self.required_model_params) - set(['has_elevator']))
         self.load_model(self, self.model_path)
 
     def load_model(self, path):
         """Метод загрузки модели"""
         try:
-            self.model = joblib.load(path)
+            self.model = load(self.model_pathpath)
+            self.pipeline =
 
         except Exception as e:
             print(f"Failed to load model: {e}")
@@ -51,3 +68,18 @@ class FlatPriceHandler:
         else:
             print("Missing parameters: ", given_params - required_params)
             return False
+
+    def handle(self, params):
+        try:
+            if not self.validate_params(params):
+                response = {"Error": "Problem with parameters"}
+            else:
+                model_params_df = keep_training_params #keep onlyfields we trained on
+                model_params_df[self.num_cols] = remove_outliers(model_params_df, self.num_cols)
+                transformed_params_df = self.pipeline.transform(transformed_params_df)
+                price_prediction = self.model.predict(transformed_params_df)
+                response = {'price prediction': price_prediction}
+        except Exception as e:
+            return {"Error": "Problem with request"}
+        else:
+            return response
